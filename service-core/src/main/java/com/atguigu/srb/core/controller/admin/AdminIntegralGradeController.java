adb0abc96cea7df95921a3ecea475a678744c50f
Fang <857559565@qq.com>
2022-07-10 17:09:08 +0800

    实现积分等级接口

---
+++ service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -0,0 +1,34 @@
+package com.atguigu.srb.core.controller.admin;
+
+
+import com.atguigu.srb.core.pojo.entity.IntegralGrade;
+import com.atguigu.srb.core.service.IntegralGradeService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <p>
+ * 积分等级表 管理员制器
+ * </p>
+ *
+ * @author FYQ
+ * @since 2022-07-10
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("admin/core/integralGrade")
+public class AdminIntegralGradeController {
+    @Resource
+    private IntegralGradeService integralGradeService;
+
+    @GetMapping("/list")
+    public List<IntegralGrade> listAll() {
+        return integralGradeService.list();
+    }
+}
+
