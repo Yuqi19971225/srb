20d1b95d251743562a379736fe5c96ad8245e8da
Fang <857559565@qq.com>
2022-08-12 20:13:28 +0800

    实现Redis

--- service-core/src/main/java/com/atguigu/srb/core/service/impl/DictServiceImpl.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/impl/DictServiceImpl.java
@@ -9,13 +9,19 @@ import com.atguigu.srb.core.service.DictService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import io.swagger.models.auth.In;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.ibatis.reflection.ExceptionUtil;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -25,8 +31,12 @@ import java.util.List;
  * @author FYQ
  * @since 2022-07-10
  */
+@Slf4j
 @Service
 public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Transactional(rollbackFor = {Exception.class})
     @Override
@@ -49,12 +59,33 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
 
     @Override
     public List<Dict> listByParentId(Long parentId) {
+        List<Dict> dictList = null;
+        try {
+            //首先在Redis中查询数据是否存在
+            dictList = (List<Dict>) redisTemplate.opsForValue().get("srb:core:dictList" + parentId);
+            if (dictList != null) {
+                log.info("从redis中取值");
+                return dictList;
+            }
+        } catch (Exception e) {
+            log.error("redis服务器异常：" + ExceptionUtils.getStackTrace(e));
+        }
+
+        log.info("从数据库中取值");
         QueryWrapper<Dict> dictQueryWrapper = new QueryWrapper<>();
         dictQueryWrapper.eq("parent_id", parentId);
-        List<Dict> dictList = baseMapper.selectList(dictQueryWrapper);
+        dictList = baseMapper.selectList(dictQueryWrapper);
         dictList.forEach(dict -> {
             dict.setHasChildren(this.hasChildren(dict.getId()));
         });
+
+        //将数据存入redis
+        try {
+            redisTemplate.opsForValue().set("srb:core:dictList" + parentId, dictList, 5, TimeUnit.MINUTES);
+            log.info("存入redis");
+        } catch (Exception e) {
+            log.error("redis服务器异常：" + ExceptionUtils.getStackTrace(e));
+        }
         return dictList;
     }
 
